ea4d756b83a585e40fdb7d86edcacd3f930f13ab
Marcos Bernardo da Silva Lobo <151075707+bernardolobo06@users.noreply.github.com>
2025-03-20 17:22:15 +0000

    feat: add string length and copy functions, refactor string handling in multiplication

--- Problema B/main.c
+++ Problema B/main.c
@@ -1,5 +1,6 @@
+// Final Version: 2025-03-20 by Marcos Bernardo da Silva Lobo <a110959@alunos.uminho.pt>
+
 #include <stdio.h>
-#include <string.h>
 
 int toInt (char c) {
     return c - '0';
@@ -9,9 +10,29 @@ char toChar (int c) {
     return c + '0';
 }
 
+int slen (char str[]) {
+    int i = 0;
+    while (str[i] != '\0') i++;
+    return i;
+}
+
+void copy (char dest[], char orig[]) {
+    int i;
+    
+    //copiar a string orig para a string temp	
+    char temp[30];
+    for (int i = 0; i < 30; i++)
+        temp[i] = orig[i];
+    
+    //copiar a string temp para a string dest
+    for (i = 0; temp[i] != '\0'; i++)
+        dest[i] = temp[i];  //assim o caracter '\0' não é copiado
+    dest[i] = '\0';
+}
+
 void reverse (char* str) {
     int i = 0;
-    int j = strlen(str) - 1;
+    int j = slen(str) - 1;
 
     while (i < j) {
         char c = str[i];
@@ -29,7 +50,7 @@ void rem (char N[]) {
 
     //remover os zeros à esquerda
     if (k > 0) {
-        int len = strlen(N) + 1; // +1 para o '\0'
+        int len = slen(N) + 1; // +1 para o '\0'
         for (int i = 0; i < len - k; i++) {
             N[i] = N[i + k];
         }
@@ -37,7 +58,7 @@ void rem (char N[]) {
 }
 
 void multiplicacao (char n1[], char n2[], char res[]) {
-    int len1 = strlen(n1), len2 = strlen(n2), len = len1+len2;
+    int len1 = slen(n1), len2 = slen(n2), len = len1+len2;
     //inverter as strings numéricas
     reverse(n1); reverse(n2);
 
@@ -70,7 +91,7 @@ void multiplicacao (char n1[], char n2[], char res[]) {
 }
 
 int compare (char n1[], char n2[]) {
-    int len1 = strlen(n1), len2 = strlen(n2);
+    int len1 = slen(n1), len2 = slen(n2);
     if (len1 > len2) return 1;
     if (len1 < len2) return -1;
     for (int i = 0; i < len1; i++) {
@@ -81,7 +102,7 @@ int compare (char n1[], char n2[]) {
 }
 
 void subprod (char N[], int S, char res[]) {
-    char prod[1001]; int len = strlen(N) - S;
+    char prod[1001] = ""; int len = slen(N) - S;
 
     for (int index = 0; index <= len; index++) {
         // Inicializar res com o primeiro elemento de N
@@ -94,30 +115,30 @@ void subprod (char N[], int S, char res[]) {
             aux[0] = N[index + i];
             aux[1] = '\0';
             multiplicacao(res, aux, res);
-            if (strlen(res) > strlen(prod)) strcpy(prod, res);
-            else if (strlen(res) == strlen(prod)) {
-                if (compare(res, prod) > 0) strcpy(prod, res);
+            if (slen(res) > slen(prod)) copy(prod, res);
+            else if (slen(res) == slen(prod)) {
+                if (compare(res, prod) > 0) copy(prod, res);
             }
         }
     }
 
-    strcpy(res, prod);
+    copy(res, prod);
 }
 
 int main () {
     int C;
     if (scanf("%d", &C) != 1) return 1;
 
+    char res[C][1001];
     for (int i = 0; i < C; i++) {
-        char N[1001], res[1001]; int S;
+        char N[1001]; int S;
         if (scanf(" %s %d", N, &S) != 2) return 1;
-        subprod(N, S, res);
-       
-        for (int j = 0; res[j] != '\0'; j++) {
-            printf("%c", res[j]);
-        }
+        subprod(N, S, res[i]);
+    }
+    for (int j = 0; j < C; j++) {
+        printf("%s", res[j]);
         printf("\n");
     }
-    
+
     return 0;
 }
